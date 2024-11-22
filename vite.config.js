6f962d8e26c3aebfc8712741c3e2d2b2e56fc125
Henrique Fernandes <henriquefernandes.gouveia@gmail.com>
2024-11-22 17:47:14 -0300

    config de deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Define a porta do servidor de desenvolvimento
+  },
+  build: {
+    outDir: 'dist', // Define a pasta de saída
+  },
+});
